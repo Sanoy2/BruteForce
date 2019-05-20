e8b3fdaaa3bcaad23e783a1c920c8c08a801d6aa
Krzysztof Tomkow <krzysztof.tomkow@gmail.com>
2019-05-20 21:26:17 +0200

    Updated run

--- cpp/run.sh
+++ cpp/run.sh
@@ -2,6 +2,6 @@
 
 chars='qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()-_=+[]{};:,./<>?'
 length=4
-path_to_file=~/Thesis/BruteForce/just_numbers_max_4.txt
+path_to_file=~/Thesis/BruteForce/mix_of_letters_numbers_specials_max_4.txt
 
 ./a.out $chars $length $path_to_file
